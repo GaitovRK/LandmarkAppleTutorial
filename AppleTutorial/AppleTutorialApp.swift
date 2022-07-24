311c01d99f8461d5e1a4b34fc7f2b87c1006b97d
Rashid Gaitov <rashidgaitov@gmail.com>
2022-07-24 15:50:04 +0300

    Initial Commit

---
+++ AppleTutorial/AppleTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTutorialApp.swift
+//  AppleTutorial
+//
+//  Created by Rashid Gaitov on 24.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleTutorial
+//
+//  Created by Rashid Gaitov on 24.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
