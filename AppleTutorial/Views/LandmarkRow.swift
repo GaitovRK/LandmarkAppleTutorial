70cb7acdfd809ae1bb25f3a24225801c3711339c
Rashid Gaitov <rashidgaitov@gmail.com>
2022-07-25 22:37:41 +0300

    Added screen with a list of landmarks

--- AppleTutorial/Views/LandmarkRow.swift
+++ AppleTutorial/Views/LandmarkRow.swift
@@ -27,6 +27,14 @@ struct LandmarkRow: View {
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarks[1])
+        Group {
+            
+            LandmarkRow(landmark: landmarks[1])
+            LandmarkRow(landmark: landmarks[2])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+
+    
+
     }
 }
