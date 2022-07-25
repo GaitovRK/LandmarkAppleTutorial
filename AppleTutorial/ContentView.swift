4c44111fb2be23d5277b1e3255e80e9cab5b86f4
Rashid Gaitov <rashidgaitov@gmail.com>
2022-07-25 14:08:59 +0300

    Finished UI

--- AppleTutorial/ContentView.swift
+++ AppleTutorial/ContentView.swift
@@ -9,8 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            ImageView()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Charyn Canyon")
+                    .font(.title)
+                
+                HStack {
+                    Text("Charyn, Tamerlik")
+                    Spacer()
+                    Text("Kazakhstan")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("Charyn Canyon is a national park in Kazakhstan, stretching along the Charyn River, including Charyn Canyon. Special thanks to my love for showing me such a beautiful place.")
+                    .font(.body)
+                
+            }
             .padding()
+            
+            Spacer()
+        }
     }
 }
 
---
+++ AppleTutorial/ImageView.swift
@@ -0,0 +1,27 @@
+//
+//  ImageView.swift
+//  AppleTutorial
+//
+//  Created by Rashid Gaitov on 25.07.2022.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Image("CharynCanyon")
+            .resizable()
+            .clipShape(Circle())
+            .aspectRatio(contentMode: .fit)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView()
+    }
+}
